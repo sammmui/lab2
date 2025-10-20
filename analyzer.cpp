6e998df7ba5c756a831f55703b64dab79f7f0dc0
Samui. <baleevamasha64@gmail.com>
2025-10-20 22:07:03 +0300

    Update analyzer.cpp

--- analyzer.cpp
+++ analyzer.cpp
@@ -1,3 +1,8 @@
 #include "analyzer.h"
 
 analyzer::analyzer() {}
+std::vector<int> analyzer::generate_data(size_t n) {
+    std::vector<int> v(n);
+     std::uniform_int_distribution<int> dist(0, 1000);
+    for (auto& x : v) x = dist(gen);
+    return v;
