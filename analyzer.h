32e25b133c3914d60ef6b801466ad40006d29f33
Samui. <baleevamasha64@gmail.com>
2025-10-20 23:33:25 +0300

    Update analyzer.h

--- analyzer.h
+++ analyzer.h
@@ -13,12 +13,19 @@ public:
     void run_tests();
 
 private:
-std::vector<int> generate_data(size_t n);
+    std::vector<int> generate_data(size_t n);
     bool predicate(int x);
     void test_library_algorithms(const std::vector<int>& data);
-       void test_parallel_algorithm(const std::vector<int>& data);
+    void test_parallel_algorithm(const std::vector<int>& data);
     bool custom_parallel_all_of(const std::vector<int>& data, size_t threads);
 
+
     template<typename Func>
-    double measure_time(Func f);
+    double measure_time(Func f) {
+        auto start = std::chrono::high_resolution_clock::now();
+        f();
+        auto end = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<double> diff = end - start;
+        return diff.count();
+    }
 };
