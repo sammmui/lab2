75f67ab4290f18b68a6f26925e3fdc73576cae28
Samui. <baleevamasha64@gmail.com>
2025-10-20 21:59:29 +0300

    Update analyzer.h

--- analyzer.h
+++ analyzer.h
@@ -11,3 +11,9 @@ private:
 std::vector<int> generate_data(size_t n);
     bool predicate(int x);
     void test_library_algorithms(const std::vector<int>& data);
+       void test_parallel_algorithm(const std::vector<int>& data);
+    bool custom_parallel_all_of(const std::vector<int>& data, size_t threads);
+
+    template<typename Func>
+    double measure_time(Func f);
+};
