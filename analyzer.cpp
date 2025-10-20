e24d77b32e170767586b63815597387487bfae64
Samui. <baleevamasha64@gmail.com>
2025-10-20 23:35:42 +0300

    Update analyzer.cpp

--- analyzer.cpp
+++ analyzer.cpp
@@ -12,15 +12,6 @@ std::vector<int> analyzer::generate_data(size_t n) {
       return x <= 900;
 }
 
-
-template<typename Func>
-double analyzer::measure_time(Func f) {
-    auto start = std::chrono::high_resolution_clock::now();
-    f();
-    auto end = std::chrono::high_resolution_clock::now();
-    std::chrono::duration<double> diff = end - start;
-    return diff.count();
-}
 void analyzer::test_library_algorithms(const std::vector<int>& data) {
     std::cout << "\n=== library algorithms all_of ===\n";
     std::cout << std::setw(25) << "policy" << std::setw(15) << "time(second)" << "\n";
